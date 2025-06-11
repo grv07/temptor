456e162a7eaaea4dd623a28a23acb2157fd49dc6
Gaurav Tyagi <grvtyagi22@gmail.com>
2025-06-11 11:57:57 +0530

    Auth user with hash pass

--- migration/src/m20250601_084832_create_user.rs
+++ migration/src/m20250601_084832_create_user.rs
@@ -11,17 +11,18 @@ impl MigrationTrait for Migration {
         manager
             .create_table(
                 Table::create()
-                    .table(User::Table)
+                    .table(Users::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(User::Id)
+                        ColumnDef::new(Users::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(User::Name).string().not_null())
-                    .col(ColumnDef::new(User::Email).string().not_null())
+                    .col(ColumnDef::new(Users::Name).string().not_null())
+                    .col(ColumnDef::new(Users::Email).string().not_null())
+                    .col(ColumnDef::new(Users::Password).string().not_null())
                     .to_owned(),
             )
             .await
@@ -31,16 +32,17 @@ impl MigrationTrait for Migration {
         // Replace the sample below with your own migration scripts
 
         manager
-            .drop_table(Table::drop().table(User::Table).to_owned())
+            .drop_table(Table::drop().table(Users::Table).to_owned())
             .await
     }
 }
 
 /// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
-enum User {
+enum Users {
     Table,
     Id,
     Name,
     Email,
+    Password,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
 mod redis_client;
 
+// use ::entity::users::{self as UsersEntity};
+use argon2::password_hash::Salt;
+use argon2::{Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
 use axum::{
     Json, Router,
     extract::{Path, State},
     http::StatusCode,
     response::IntoResponse,
-    routing::get,
+    routing::{get, post},
 };
+
+use ::entity::prelude::Users as UsersEntity;
+use ::entity::users;
+use base64::Engine;
+use migration::sea_orm::{self, ActiveValue, DatabaseConnection};
 use r2d2::Pool;
 use redis::Commands;
-use serde::{Deserialize, Serialize};
+use sea_orm::entity::prelude::*;
+use sea_orm::*;
+
 use serde_json;
 use tokio::net::TcpListener;
 
 #[derive(Clone)]
 struct AppState {
-    redis_pool: Pool<redis::Client>,
+    redis: Pool<redis::Client>,
+    db: DatabaseConnection,
 }
 
 #[tokio::main]
@@ -25,13 +36,26 @@ async fn main() {
 
     // Redis connections pool
     let client = redis::Client::open("redis://127.0.0.1/").unwrap();
-    let pool = r2d2::Pool::builder().build(client).unwrap();
+    let pool = r2d2::Pool::builder()
+        .build(client)
+        .expect("Error: Unable to connect with redis ...");
 
-    let state = AppState { redis_pool: pool };
+    let conn: DatabaseConnection =
+        // postgres://postgres:admin@localhost/temptor
+        sea_orm::Database::connect("postgres://postgres:admin@localhost/temptor")
+            .await
+            .unwrap();
+
+    let state = AppState {
+        redis: pool,
+        db: conn,
+    };
 
     let router = Router::new()
         .route("/", get(ping))
-        .route("/user/{id}", get(get_user).post(create_user))
+        .route("/user", post(create_user))
+        .route("/user/{id}", get(get_user))
+        .route("/login", post(login))
         .with_state(state);
 
     println!("Server starts at: {addr}");
@@ -43,35 +67,76 @@ async fn ping() -> &'static str {
     "pong"
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-struct User {
-    id: u64,
-    name: String,
-    email: String,
-}
-
 async fn get_user(Path(id): Path<String>, State(state): State<AppState>) -> impl IntoResponse {
-    let mut rd = state.redis_pool.get().unwrap();
+    let mut rd = state.redis.get().unwrap();
     let user_id = format!("user:{id}");
     let user: String = rd.hget("users", user_id).unwrap();
 
-    let data: User = serde_json::from_str(user.as_str()).unwrap();
+    let data: users::Model = serde_json::from_str(user.as_str()).unwrap();
 
     Json(data)
 }
 
 async fn create_user(
     State(state): State<AppState>,
-    Json(user): Json<User>,
-) -> (StatusCode, Json<User>) {
-    let id = format!("user:{}", user.id);
+    Json(user): Json<users::Model>,
+) -> Result<(StatusCode, Json<String>), StatusCode> {
+    println!("Create user");
+    let mut rd = state.redis.get().unwrap();
 
-    let mut rd = state.redis_pool.get().unwrap();
+    let argon2 = Argon2::default();
 
-    let data = serde_json::to_string(&user).unwrap();
-    let _: () = rd.hset("users", id, data).unwrap();
+    let b_salt = base64::engine::general_purpose::STANDARD.encode(format!("{}DEF", user.name));
+    let salt_hash = Salt::from_b64(&b_salt).expect("Salt from b64");
 
-    (StatusCode::OK, Json::from(user))
+    let hash_pass = argon2
+        .hash_password(user.password.as_bytes(), salt_hash)
+        .expect("Password is not valid")
+        .to_string();
+
+    println!("Create user: {hash_pass:?}");
+
+    let user = users::ActiveModel {
+        id: NotSet,
+        name: ActiveValue::Set(user.name),
+        email: ActiveValue::Set(user.email),
+        password: ActiveValue::Set(hash_pass),
+    };
+
+    if let Ok(user) = user.save(&state.db).await {
+        let user = user.try_into_model().unwrap();
+
+        let data = serde_json::to_string_pretty(&user).unwrap();
+
+        let id = format!("user:{}", user.id);
+        println!("id: {id:?}");
+
+        Ok((StatusCode::OK, Json(data)))
+    } else {
+        Err(StatusCode::EXPECTATION_FAILED)
+    }
 }
 
-// async fn add_template(State(state): State<AppState>) {}
+async fn login(State(state): State<AppState>, Json(user): Json<users::Model>) -> StatusCode {
+    let find_user = UsersEntity::find()
+        .filter(
+            Condition::any()
+                .add(users::Column::Name.eq(user.name.clone()))
+                .add(users::Column::Email.eq(user.email.clone())),
+        )
+        .one(&state.db)
+        .await
+        .unwrap()
+        .unwrap();
+
+    let argon2 = Argon2::default();
+
+    let hash_pass = PasswordHash::new(&find_user.password).unwrap();
+
+    if let Ok(_) = argon2.verify_password(user.password.as_bytes(), &hash_pass) {
+        println!("Password verified ... ");
+        StatusCode::OK
+    } else {
+        StatusCode::UNAUTHORIZED
+    }
+}
